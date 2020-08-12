603c53271d78a02aa9d7973cefd0a6b6e3be990a
mdudyala <mdudyala@nmdp.org>
2020-08-11 20:21:52 -0500

    reporting app supports STU3 and DSTU2 external FHIR versions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 export class AppConfig {
   // Define all the constants of the app
 
-  public static backendOrigin="https://dev-api.nmdp.org";
+  public static backendOrigin = "https://dev-api.nmdp.org";
+
+  public static getPatientIdentifier =
+    "/api/epic/2015/Common/Patient/GetPatientIdentifiers/Patient/Identifiers";
 
   //EPIC Client ID's
 
-  public static client_id = "b5aaa0c6-6909-4473-a11e-fa7492cdce6d";
+  public static client_id = "fe30e164-a265-4f71-9b14-570be8cb3f27";
 
   //AWSEnv
   public static epic_oauth_redirect_url =
-    "https://cibmtr-fhir-ehr-dev.nmdp.org";
-   
+    "https://cibmtr-fhir-ehr-frontend-qa.aws.nmdp.org";
+
   //CIBMTR Namespaces
 
   public static cibmtr_crid_namespace = "http://cibmtr.org/identifier/CRID";
@@ -24,23 +27,20 @@ export class AppConfig {
   //Middleware URLs
 
   public static crid_service_endpoint =
-    "https://dev-api.nmdp.org/cibmtrehrclientbackend/v2/CRID";
+    "https://qa-internal-api.nmdp.org/cibmtrehrclientbackendqa/v1/CRID";
 
   public static observation_codes =
-    "code=https://cdebrowser.nci.nih.gov|88,https://cdebrowser.nci.nih.gov|64159,https://cdebrowser.nci.nih.gov|2003853,https://cdebrowser.nci.nih.gov|2603692,https://cdebrowser.nci.nih.gov|2787346,https://cdebrowser.nci.nih.gov|2787385,https://cdebrowser.nci.nih.gov|2787392,https://cdebrowser.nci.nih.gov|2787396,https://cdebrowser.nci.nih.gov|2787403,https://cdebrowser.nci.nih.gov|2787415,https://cdebrowser.nci.nih.gov|2787423,https://cdebrowser.nci.nih.gov|2787453,https://cdebrowser.nci.nih.gov|2787459,https://cdebrowser.nci.nih.gov|2787467,https://cdebrowser.nci.nih.gov|2787484,https://cdebrowser.nci.nih.gov|2787521,https://cdebrowser.nci.nih.gov|2787539,https://cdebrowser.nci.nih.gov|2787565,https://cdebrowser.nci.nih.gov|2787579,https://cdebrowser.nci.nih.gov|2787616,https://cdebrowser.nci.nih.gov|2787667,https://cdebrowser.nci.nih.gov|2787671,https://cdebrowser.nci.nih.gov|2787840,https://cdebrowser.nci.nih.gov|2787856,https://cdebrowser.nci.nih.gov|2787862,https://cdebrowser.nci.nih.gov|2787870,https://cdebrowser.nci.nih.gov|2787872,https://cdebrowser.nci.nih.gov|2787874,https://cdebrowser.nci.nih.gov|2793688,https://cdebrowser.nci.nih.gov|2793723,https://cdebrowser.nci.nih.gov|2793723,https://cdebrowser.nci.nih.gov|2797618,https://cdebrowser.nci.nih.gov|2797645,https://cdebrowser.nci.nih.gov|2797671,https://cdebrowser.nci.nih.gov|2860477,https://cdebrowser.nci.nih.gov|2861433,https://cdebrowser.nci.nih.gov|3578143,https://cdebrowser.nci.nih.gov|4492414,https://cdebrowser.nci.nih.gov|492418,https://cdebrowser.nci.nih.gov|4492434,https://cdebrowser.nci.nih.gov|4492471,https://cdebrowser.nci.nih.gov|4492511,https://cdebrowser.nci.nih.gov|4492571,https://cdebrowser.nci.nih.gov|4492575";
+    "code=https://cdebrowser.nci.nih.gov|2003853,https://cdebrowser.nci.nih.gov|2603692,https://cdebrowser.nci.nih.gov|2787385,https://cdebrowser.nci.nih.gov|2787392,https://cdebrowser.nci.nih.gov|2787396,https://cdebrowser.nci.nih.gov|2787403,https://cdebrowser.nci.nih.gov|2787415,https://cdebrowser.nci.nih.gov|2787423,https://cdebrowser.nci.nih.gov|2787453,https://cdebrowser.nci.nih.gov|2787459,https://cdebrowser.nci.nih.gov|2787467,https://cdebrowser.nci.nih.gov|2787484,https://cdebrowser.nci.nih.gov|2787521,https://cdebrowser.nci.nih.gov|2787539,https://cdebrowser.nci.nih.gov|2787565,https://cdebrowser.nci.nih.gov|2787579,https://cdebrowser.nci.nih.gov|2787616,https://cdebrowser.nci.nih.gov|2787667,https://cdebrowser.nci.nih.gov|2787671,https://cdebrowser.nci.nih.gov|2787840,https://cdebrowser.nci.nih.gov|2787856,https://cdebrowser.nci.nih.gov|2787862,https://cdebrowser.nci.nih.gov|2787870,https://cdebrowser.nci.nih.gov|2787872,https://cdebrowser.nci.nih.gov|2787874,https://cdebrowser.nci.nih.gov|2793688,https://cdebrowser.nci.nih.gov|2797618,https://cdebrowser.nci.nih.gov|2797645,https://cdebrowser.nci.nih.gov|2797671,https://cdebrowser.nci.nih.gov|2860477,https://cdebrowser.nci.nih.gov|2861433,https://cdebrowser.nci.nih.gov|3578143,https://cdebrowser.nci.nih.gov|4492414,https://cdebrowser.nci.nih.gov|492418,https://cdebrowser.nci.nih.gov|4492434,https://cdebrowser.nci.nih.gov|4492471,https://cdebrowser.nci.nih.gov|4492511,https://cdebrowser.nci.nih.gov|4492571,https://cdebrowser.nci.nih.gov|4492575";
 
   public static cibmtr_fhir_base_url =
-    "https://dev-api.nmdp.org/cibmtrehrclientbackend/v2/Patient?identifier=";
+    "https://qa-internal-api.nmdp.org/cibmtrehrclientbackendqa/v1/Patient?identifier=";
 
   public static cibmtr_fhir_update_url =
-    "https://dev-api.nmdp.org/cibmtrehrclientbackend/v2/";  
+    "https://qa-internal-api.nmdp.org/cibmtrehrclientbackendqa/v1/";
 
   public static codes = [
-    "88",
-    "64159",
     "2003853",
     "2603692",
-    "2787346",
     "2787385",
     "2787392",
     "2787396",
@@ -65,8 +65,6 @@ export class AppConfig {
     "2787872",
     "2787874",
     "2793688",
-    "2793723",
-    "2793723",
     "2797618",
     "2797645",
     "2797671",
@@ -79,6 +77,6 @@ export class AppConfig {
     "4492471",
     "4492511",
     "4492571",
-    "4492575"
+    "4492575",
   ];
 }
--- src/app/patient/patient.resolver.ts
+++ src/app/patient/patient.resolver.ts
@@ -2,61 +2,95 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Resolve
+  Resolve,
 } from "@angular/router";
 import { BsModalRef } from "ngx-bootstrap/modal";
-import { forkJoin } from "rxjs";
+import { forkJoin, throwError } from "rxjs";
 import { IPatientContext } from "../model/patient.";
 import { PatientService } from "./patient.service";
 import { LocalStorageService } from "angular-2-local-storage";
+import { AppConfig } from "../app.config";
+import { IIdentifiers } from "../model/identifiers";
+import { HttpHeaders } from "@angular/common/http";
+import { NmdpWidget } from "@nmdp/nmdp-login/Angular/service/nmdp.widget";
+import { HttpClient } from "@angular/common/http";
+
 @Injectable()
 export class PatientResolver implements Resolve<IPatientContext[]> {
   bsModalRef: BsModalRef;
   constructor(
     private patientDetailService: PatientService,
-    private _localStorageService: LocalStorageService
+    private _localStorageService: LocalStorageService,
+    private http: HttpClient,
+
+    private nmdpWidget: NmdpWidget
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
     //Commented the below code as EPIC STU3 identifiers
 
     //Post call to get the STU3 patient id by retreving the patient identifier from local storage
-    /*let body = {
-      PatientID: this._localStorageService.get("patient"),
-      PatientIDType: "FHIR",
 
-      UserID: "1",
-      UserIDType: "External"
-    };
+    let issurl: string = this._localStorageService.get("iss");
 
-    let ehrHeaders: HttpHeaders = new HttpHeaders()
-      .set("Content-Type", "application/json")
-      .set("Accept", "application/json")
-      .set(
-        "Authorization",
-        `Bearer ${this._localStorageService.get("accessToken")}`
+    if (issurl.includes("DSTU2")) {
+      let body = {
+        PatientID: this._localStorageService.get("patient"),
+        PatientIDType: "FHIR",
+        UserID: "1",
+        UserIDType: "External",
+      };
+
+      let ehrHeaders: HttpHeaders = new HttpHeaders()
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .set(
+          "Authorization",
+          `Bearer ${this._localStorageService.get("accessToken")}`
+        );
+
+      //format - https://domain/IC-instance/api/epic/2015/Common/Patient/GetPatientIdentifiers/Patient/Identifiers
+
+      let epicidentifier: string = this._localStorageService.get("iss");
+
+      let epic_identifier_url = epicidentifier.replace(
+        /\/api\/.*/,
+        AppConfig.getPatientIdentifier
       );
 
-    return this.http
-      .post(AppConfig.patient_identifiers, body, {
-        headers: ehrHeaders
-      })
-      .switchMap((response: IIdentifiers) => {
-        let filteredIdentifiers = response.Identifiers.filter(
-          identifier => identifier.IDType === "FHIR STU3"s
-        );*/
+      return this.http
+        .post(decodeURIComponent(epic_identifier_url.toString()), body, {
+          headers: ehrHeaders,
+        })
+        .subscribe(
+          (response: IIdentifiers) => {
+            let filteredIdentifiers = response.Identifiers.filter(
+              (identifier) => identifier.IDType === "FHIR STU3"
+            );
 
-    // if (!this.nmdpWidget.isLoggedIn()) {
-    //   return;
-    // }
+            // If there is one
+            if (filteredIdentifiers) {
+              let ID = filteredIdentifiers[0].ID;
+              return forkJoin([
+                this.patientDetailService.getPatient(ID),
+                this.patientDetailService.getObservation(ID),
+              ]);
+            } else {
+              // there is no patient id hence throw the error
+              return window.alert("Invalid Patient Identifier");
+            }
+          },
+          (error) => {
+            throwError(error);
+          }
+        );
+    } else {
+      let id = this._localStorageService.get("patient");
 
-    //let decodedValue = this.getDecodedAccessToken(this.nmdpWidget.getAccessToken());
-
-    let id = this._localStorageService.get("patient");
-
-    return forkJoin([
-      this.patientDetailService.getPatient(id),
-      this.patientDetailService.getObservation(id)
-    ]);
+      return forkJoin([
+        this.patientDetailService.getPatient(id),
+        this.patientDetailService.getObservation(id),
+      ]);
+    }
   }
 }
