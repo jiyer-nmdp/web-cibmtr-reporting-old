4b9bc91c8a5f80df80b33940da10c1ad5143f0fd
mdudyala-nmdp <mdudyala@nmdp.org>
2021-01-18 21:58:14 -0600

    Changes to allow Mocked EHR Labs and Vitals responses

--- src/app/mock/mock.http.interceptor.ts
+++ src/app/mock/mock.http.interceptor.ts
@@ -7,14 +7,27 @@ import {
   HttpResponse,
 } from "@angular/common/http";
 import { Observable, of } from "rxjs";
-import patient from "./patient.json";
 
-const patient_url = [
+import * as patient from "../data/patient.json";
+import * as labs from "../data/labs.json";
+import * as vitals from "../data/vitals.json";
+
+const ehr_url = [
   {
     //
     url:
       "https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/STU3/Patient/endFZe2sorgK5yH219Tqbsw3",
-    json: "patient",
+    json: patient,
+  },
+  {
+    url:
+      "https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/STU3/Observation?category=laboratory&patient=endFZe2sorgK5yH219Tqbsw3",
+    json: labs,
+  },
+  {
+    url:
+      "https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/STU3/Observation?category=vital-signs&patient=endFZe2sorgK5yH219Tqbsw3",
+    json: vitals,
   },
 ];
 
@@ -26,7 +39,7 @@ export class HttpMockRequestInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    for (const element of patient_url) {
+    for (const element of ehr_url) {
       if (request.url === element.url) {
         console.log("Loaded from json : " + request.url);
         return of(
