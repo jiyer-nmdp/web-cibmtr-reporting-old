61f9fd359b831bff293158c87cc05352f8cc9bd9
mdudyala-nmdp <mdudyala@nmdp.org>
2021-03-01 00:19:17 -0600

    merge conflicts updated

--- src/app/observation.corecharacteristics/observation.corecharacteristics.service.ts
+++ src/app/observation.corecharacteristics/observation.corecharacteristics.service.ts
@@ -3,8 +3,8 @@ import { AppConfig } from "../app.config";
 import { Observable } from "rxjs";
 import { CustomHttpClient } from "../client/custom.http.client";
 import { UtilityService } from "../utility.service";
-import { catchError, map } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { map } from "rxjs/operators";
+import { EMPTY } from "rxjs";
 
 @Injectable()
 export class ObservationCoreService {
@@ -30,14 +30,9 @@ export class ObservationCoreService {
   getBundleObservable(bundle) {
     return this.http
       .post(AppConfig.cibmtr_fhir_update_url + "Bundle", bundle)
-      .pipe(
-        map(() => bundle),
-        catchError((err) => {
-          console.log("caught mapping error and rethrowing", err);
-          return throwError(bundle);
-        })
-      )
-      .retry(1);
+      .pipe(map(() => bundle))
+      .retry(1)
+      .catch(() => EMPTY);
   }
 
   // Below method submit updated records to the cibmtr
