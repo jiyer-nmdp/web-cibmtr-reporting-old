6ad1839c16727bbda0dbf397a06c9b9630a955ac
jiyer2 <jiyer2@nmdp.org>
2021-04-19 18:44:30 -0500

    Correct okta_env_name to replace in environment files
    Signed-off-by: jiyer2 <jiyer2@nmdp.org>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   okta_setup: {
     client_id: window["env"]["okta_client_id"],
     scopes: [window["env"]["okta_scopes"]],
-    environment : window["env"]["env_name"]
+    environment : window["env"]["okta_env_name"]
   },
   ehr_redirect_url: window["env"]["ehr_redirect_url"],
   cibmtr_fhir_r3_url: window["env"]["cibmtr_fhir_r3_url"]
