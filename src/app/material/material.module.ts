0670acb2af69cd4c8449d2b5f7f15cdcb4412c11
mdudyala-nmdp <mdudyala@nmdp.org>
2021-04-22 15:48:15 -0500

    Migrated MaterialModule 8.2.3

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { MatBadgeModule, MatIconModule } from "@angular/material";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatIconModule } from "@angular/material/icon";
 
 const MaterialComponents = [MatIconModule, MatBadgeModule];
 
