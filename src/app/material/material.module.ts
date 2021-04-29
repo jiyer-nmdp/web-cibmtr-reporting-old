1a107e9271317bc14e2d052521b964c0882f4a22
mdudyala-nmdp <mdudyala@nmdp.org>
2021-04-28 23:59:44 -0500

    uninstalled Material Module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { MatBadgeModule } from "@angular/material/badge";
-import { MatIconModule } from "@angular/material/icon";
+// import { MatBadgeModule } from "@angular/material/badge";
+// import { MatIconModule } from "@angular/material/icon";
 
-const MaterialComponents = [MatIconModule, MatBadgeModule];
+//const MaterialComponents = [MatIconModule, MatBadgeModule];
 
 @NgModule({
-  imports: [MaterialComponents],
-  exports: [MaterialComponents],
+  // imports: [MaterialComponents],
+  // exports: [MaterialComponents],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MaterialModule {}
