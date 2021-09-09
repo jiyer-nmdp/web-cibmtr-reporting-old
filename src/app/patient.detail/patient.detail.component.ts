96f5bfdbd091da8e7f72e0f137df8bb957573a1c
mdudyala-nmdp <mdudyala@nmdp.org>
2021-09-09 10:51:48 -0500

    Additional Changes for Confirmation

--- src/app/patient.detail/patient.detail.component.ts
+++ src/app/patient.detail/patient.detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, HostListener } from "@angular/core";
 import { Patient } from "../model/patient.";
 import { UtilityService } from "../utility.service";
+import { ConfirmationDialog } from "../confirm.dialog/confirmation-dialog";
+import { MatDialog } from "@angular/material/dialog/dialog";
+import { MatDialogRef } from "@angular/material/dialog/dialog-ref";
 
 @Component({
   selector: "app-patient.detail",
@@ -9,19 +12,19 @@ import { UtilityService } from "../utility.service";
 })
 export class PatientDetailComponent implements OnInit {
   ehrpatient: Patient;
-  labs: any;
+  //labs: any;
   psScope: string;
   crid: string;
   activeLabel: string;
   priority: any;
+  dialogRef: MatDialogRef<ConfirmationDialog>;
 
-  constructor(private utility: UtilityService) {
+  constructor(private utility: UtilityService, public dialog: MatDialog) {
     let data = utility.data;
 
-    this.labs = this.utility.bundleObservations(data.labs);
-    this.priority = this.utility.bundleObservations(data.priorityLabs);
-
+    //this.labs = this.utility.bundleObservations(data.labs);
     this.ehrpatient = JSON.parse(data.ehrpatient);
+    this.priority = this.utility.bundleObservations(data.priorityLabs);
     this.crid = data.crid;
     this.psScope = data.psScope;
   }
@@ -47,5 +50,22 @@ export class PatientDetailComponent implements OnInit {
     if (target.id != "") {
       this.activeLabel = target.id;
     }
+  }
+
+  //Confirmation on AllLabs API on demand
+  openConfirmationDialog() {
+    this.dialogRef = this.dialog.open(ConfirmationDialog, {
+      disableClose: false,
+    });
+    //Alert_messages will be maintained in constant file in future
+    this.dialogRef.componentInstance.confirmMessage =
+      "Retrieving All Labs may taken several minutes. Do you want to continue?";
+
+    this.dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // do confirmation actions
+      }
+      this.dialogRef = null;
+    });
   }
 }
